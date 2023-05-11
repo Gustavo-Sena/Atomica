9b092ab5c34f0f4bd882a5737cf1c18a2fc466e2
Gustavo-sena <gustavo.sena@sptech.school>
2023-05-11 17:53:35 -0300

    Adicionando a tela de login/cadastro ao sistea

---
+++ login/script.js
@@ -0,0 +1,25 @@
+var btn_entrar = document.querySelector("#btn_entrar");
+
+var btn_cadastrar = document.querySelector("#btn_cadastrar");
+
+var body = document.querySelector("body");
+
+btn_entrar.addEventListener("click", function(){
+    body.className = "ENTRAR-js";
+});
+
+btn_cadastrar.addEventListener("click", function(){
+    body.className = "CADASTRAR-js"
+})
+
+function cadastrar(){
+    var senha = ipt_senha.value
+    var conf_senha = ipt_conf_senha.value
+
+    if(conf_senha == senha){
+        alert("TUDO CERTO!")
+    }
+    else{
+        alert(`A confirmação da senha está diferente da senha inicial`)
+    }
+}
